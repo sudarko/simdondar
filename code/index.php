6366fe50ae196b0f12614e3342a8f9516c36184b
sudarko <darko@unej.ac.id>
2023-06-09 07:50:26 +0700

    Update index.php

--- code/index.php
+++ code/index.php
@@ -1,10 +1,4 @@
-<table border="0">
-    <tr bgcolor="#ffffff"><td align="center"valign="center">
 <img src="simdondar.jpg"/>
-    </td></tr>
-  <tr bgcolor="#ffffff"><td>
-    <?php
+<?php
 phpinfo();
 ?>
-    </td></tr>
-</table>
