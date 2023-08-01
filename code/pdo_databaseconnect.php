9a65883bf035e39c594154cc1be73a00cb49669b
sudarko <darko@unej.ac.id>
2023-08-01 14:15:47 +0700

    Update pdo_databaseconnect.php

--- code/pdo_databaseconnect.php
+++ code/pdo_databaseconnect.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'pdoconfig.php';
+require_once 'dbconfig.php';
 try {
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 echo "Connected to $dbname at $host successfully.";
