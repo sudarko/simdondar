13dafee219c5df8e07c31768fa515ca7e5be3667
sudarko <darko@unej.ac.id>
2023-08-01 14:10:45 +0700

    Update index.php

--- code/index.php
+++ code/index.php
@@ -31,12 +31,6 @@ $user = 'root';
 $pass = 'example';
 $port = "3306";
 $charset = 'utf8mb4';
-
-$options = [
-    \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
-    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-    \PDO::ATTR_EMULATE_PREPARES   => false,
-];
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
 $pdo = new \PDO($dsn, $user, $pass, $options);
 phpinfo();
