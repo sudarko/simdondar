5e0e12cd8f9504f3120eb7bc4dd960c26e640c7d
sudarko <darko@unej.ac.id>
2023-08-01 13:02:54 +0700

    Update index.php

--- code/index.php
+++ code/index.php
@@ -7,12 +7,14 @@ $username = "root";
 $password = "example";
 
 // Create connection
+/*
 $conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 } 
+*/
 echo "Connected successfully";
 phpinfo();
 ?>
