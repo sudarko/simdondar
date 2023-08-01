fa971fe15ee6a9e4aec73912f3c4baf70c5da4a0
sudarko <darko@unej.ac.id>
2023-08-01 14:28:15 +0700

    Update index.php

--- code/index.php
+++ code/index.php
@@ -1,37 +1,10 @@
 <img src="simdondar.jpg"/>
+Connect DB:<br>
+<ol>
+  <li><a href=mysql_connect.php>Old driver php-mysql</a></li>
+    <li><a href=mysqli_connect.php>New mysqli driver</a></li>
+      <li><a href=pdo_connect.php>PDO driver</a></li>
+</ol>
 <?php
-echo "try update";
-echo "test 123";
-
-$conn = mysql_connect('database', 'root', 'example');
-mysql_select_db("mysql");
-if (!$conn) {
-die('ERROR: Unable to connect: ' . mysql_error());
-}
-
-echo 'Connected to the database.<br>';
-
-mysql_close($conn);
-/**/
-$servername = "database";
-$username = "root";
-$password = "example";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
-$host = 'database';
-$db   = 'mysql';
-$user = 'root';
-$pass = 'example';
-$port = "3306";
-$charset = 'utf8mb4';
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
-$pdo = new \PDO($dsn, $user, $pass, $options);
 phpinfo();
 ?>
