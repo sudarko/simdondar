0aa6e69ee37ab23e0cab787850032bcc17452067
sudarko <darko@unej.ac.id>
2023-08-08 11:00:48 +0700

    Update index.php

--- code/index.php
+++ code/index.php
@@ -1,5 +1,6 @@
 <img src="simdondar.jpg"/>
 Test koneksi ke DB:<br>
+TEST EDIT 08-08-23<br>
 <ol>
   <li><a href=mysql_connect.php>Old driver php-mysql</a></li>
     <li><a href=mysqli_connect.php>New mysqli driver</a></li>
