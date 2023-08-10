4d7d39180baa3c9d3f71ed44073276fb4a8cc168
sudarko <darko@unej.ac.id>
2023-08-10 09:51:06 +0700

    Update index.php

--- code/index.php
+++ code/index.php
@@ -4,6 +4,8 @@ Test koneksi ke DB:<br>
 TEST EDIT 08-08-23 NEW<br>
 Update 09-08-23 <br>
 TEST autobuild/push tag:master<br>
+update docker-compose.yaml and cron-simdondar.sh, sekarang tidak perlu build dan push secara manual, tiap ada push/update di github sudah otomatis build dan push ke docker hub, selanjutnya disever UTD yang sudah dipasang cron-simdondar.sh langsung mendekteksi image baru dari docker hub dan update simdondar secara otomatis terjadi<br>
+jadi tugas developer hanya update/push digithup<br>
 <ol>
   <li><a href=mysql_connect.php>Old driver php-mysql</a></li>
     <li><a href=mysqli_connect.php>New mysqli driver</a></li>
