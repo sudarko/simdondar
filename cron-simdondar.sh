0ab8a13a84a51c787fbc87038873b726c3a2d165
sudarko <darko@unej.ac.id>
2023-08-10 09:53:28 +0700

    Update cron-simdondar.sh

--- cron-simdondar.sh
+++ cron-simdondar.sh
@@ -5,6 +5,6 @@ EOF
 then
 	echo 'No new Update'
 else
-	docker rmi `docker images |grep none |awk '{print $3}'`
 	docker-compose up -d --no-deps webserver
+ 	docker rmi `docker images |grep none |awk '{print $3}'`
 fi
