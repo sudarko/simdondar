dc49f07e97d8da970049322aa8733dc376014a2d
sudarko <darko@unej.ac.id>
2023-08-10 08:52:31 +0700

    Update index.php

--- code/index.php
+++ code/index.php
@@ -3,6 +3,7 @@ Versi 1.0<br>
 Test koneksi ke DB:<br>
 TEST EDIT 08-08-23 NEW<br>
 Update 09-08-23 <br>
+TEST autobuild/push<br>
 <ol>
   <li><a href=mysql_connect.php>Old driver php-mysql</a></li>
     <li><a href=mysqli_connect.php>New mysqli driver</a></li>
